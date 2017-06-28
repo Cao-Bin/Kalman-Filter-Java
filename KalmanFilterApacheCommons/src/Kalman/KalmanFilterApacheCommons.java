cdde41addb6f4c312927c701ea7cc5fd6142d87a
Marinus Toman <L00118908@student.lyit.ie>
2017-06-28 16:40:00 +0100

    Started implementing Kalman Filter.

---
+++ KalmanFilterApacheCommons/src/Kalman/KalmanFilterApacheCommons.java
@@ -0,0 +1,17 @@
+package Kalman;
+
+/**
+ * @author Marinus Toman
+ *  Date:  28-Jun-2017
+ */
+public class KalmanFilterApacheCommons {
+
+    /**
+     * Main method
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
